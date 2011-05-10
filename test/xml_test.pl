d29a281a64d2afc6e8f0c88835b4019c2a19a77c
Michael Harris <shiftycow@gmail.com>
2011-05-10 09:57:09 -0600

    working on XML config option

---
+++ test/xml_test.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+use strict;
+
+use XML::Simple;
+use XML::Dumper;
+use Getopt::Long;
+use Data::Dumper;
+
+my $filename = @ARGV[0];
+
+my $xml = new XML::Simple;
+
+if($filename eq undef)
+{
+    print "filename argument missing, exiting\n";
+    exit 1;
+}
+
+my $data = $xml->XMLin($filename);
+my $xml_output = XML::Dumper::pl2xml($data);
+print $xml_output;
