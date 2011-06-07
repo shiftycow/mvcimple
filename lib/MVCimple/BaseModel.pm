cc91407dbc6808dedeaa7a7d593f19df0e64d50a
David Salas <davidsalas.work@gmail.com>
2011-06-07 12:05:13 -0600

    Working on sql saving in BaseModel.pm

--- lib/MVCimple/BaseModel.pm
+++ lib/MVCimple/BaseModel.pm
@@ -7,7 +7,7 @@ use Data::Dumper;
 use DBI;
 
 #local includes
-use lib "../lib"
+use lib "../lib";
 use MVCimple::Config;
 
 #This is the base model for any model Class
@@ -84,7 +84,67 @@ sub save {
     my $dbhost = MVCimple::Config::get_config_element('dbhost');    
     my $dbpassword = MVCimple::Config::get_config_element('dbpassword');    
     my $dbsuser = MVCimple::Config::get_config_element('dbuser');    
-    my $database = MVCimple::Config::get_config_element('database');    
+    my $dbname = MVCimple::Config::get_config_element('dbname');    
+    my $dbport = MVCimple::Config::get_config_element('dbport');
+    my $table_prefix = MVCimple::Config::get_config_element('table_prefix');
+    my $driver = MVCimple::Config::get_config_element('driver');
+
+    my $modelname = $self->{'name'};
+    # my $dbh = DBI->connect("dbi:$driver:$dbname:$dbhost:$dbport",$dbuser,$dbpassword);
+    my $dbh = DBI->connect("dbi:SQLite:dbname=/tmp/database","","");
+
+    
+    my $sql = "";
+    #TODO Insert logic for UPDATE table
+    $sql .= "INSERT INTO $modelname (";
+    my $i = 0; #counter used for putting in commas
+    my $columns = scalar (keys %{$self->{columns}}); # Figure out how many columns there are
+
+    #Add column names to sql statement
+    while( my($name,$column_data) = each(%{$self->{columns}})) {
+        #$data->{$name} = $column_data->get_value();
+        $sql .= "`$name`";
+        $sql .= "," if ($i < $columns - 1);
+        $i++;
+    }
+   
+    $sql .= ") VALUES (";
+   
+    #Add ? to sql statement
+    $i = 0; 
+    while( my($name,$column_data) = each(%{$self->{columns}})) {
+        $sql .= "?";
+        #$sql .= $column_data->get_value();
+        $sql .= "," if ($i < $columns - 1);
+        $i++;
+    }
+    $sql .= ")";
+    print $sql; #DEBUG
+    
+    my $sth = $dbh->prepare($sql)
+         or die "Can't prepare SQL statement: $DBI::errstr\n";
+    #TODO return proper error
+    
+    my @data = ();    
+    while( my($name,$column_data) = each(%{$self->{columns}})) {
+       push @data, $column_data->get_value();
+    }
+    print "@data"; #DEBUG
+
+    $sth->execute(@data)
+        or die "Can't execute SQL statement: $DBI::errstr\n";
+    $dbh->commit();
+
+    $sth = $dbh->prepare("SELECT * FROM $modelname");
+    $sth->execute();     
+    ##retrieve the returned rows of data
+    my @row;
+    while (@row = $sth->fetchrow_array()){
+        print "Row: @row\n"
+    }
+    warn "Data fetching terminated early by error: $DBI::errstr\n" if $DBI::err;
+
+    #return $sql; #DEBUG
 }
 
 1;
--- lib/MVCimple/Config.pm
+++ lib/MVCimple/Config.pm
@@ -3,4 +3,9 @@
 use strict;
 package MVCimple::Config;
 
+sub get_config_element {
+    return "";
+    #TODO finish this
+}
+
 1;
