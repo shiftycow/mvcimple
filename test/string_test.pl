06c56e1c2581a69b65c90b902060d4d13326888f
David Salas <davidsalas.work@gmail.com>
2011-06-10 09:23:10 -0600

    Changed validation method of String.pm to return has with error or success message. Fixed tests to accommodate changes

--- test/string_test.pl
+++ test/string_test.pl
@@ -3,19 +3,24 @@
 # testing script for the MVCimple::String class
 #
 #
-
 use strict;
+
+use Data::Dumper;
+
 use lib "../lib";
 use MVCimple::String;
 
 my $string = new MVCimple::String("test_string",{'length'=>30, 'null'=>0});
+my $return = {};
 
 print $string->to_sql()."\n";
 print $string->to_input()."\n";
 
 $string->set_value('trololol');
-
-print $string->get_value() . " is a valid string.\n" if($string->validate());
+print Dumper($return = $string->validate());
+print $string->get_value() . " is a valid string.\n" if($return->{'error'} eq undef);
 
 $string->set_value();
-print $string->get_value() . " is a valid string.\n" if($string->validate());
+print Dumper($return = $string->validate());
+print $string->get_value() . " is a valid string.\n" if($return->{'error'} eq undef);
+
