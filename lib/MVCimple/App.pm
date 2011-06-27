81a56e29f84e6adf01bf74238b91a3d6e19ccaeb
Michael Harris <shiftycow@gmail.com>
2011-06-27 12:08:48 -0600

    Added App.pm interface to set up MVCimple environment

---
+++ lib/MVCimple/App.pm
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+use strict;
+package MVCimple::App;
+#
+#    This file is part of MVCimple.
+#
+#    MVCimple is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    MVCimple is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with MVCimple.  If not, see <http://www.gnu.org/licenses/>. 
+#
+#
+
+# wrapper for including necessary MVCimple modules for a web application
+
+use MVCimple::BaseModel;
+use MVCimple::DBConnect;
+use MVCimple::Config;
+use MVCimple::RenderView;
+
+1;
