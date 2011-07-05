bf8b63c827468676559f555f2cae68dcdfc89a67
Michael Harris <shiftycow@gmail.com>
2011-07-05 12:06:41 -0600

    added error, message, and add_greeting XSLT templates

--- samples/hello_world/public/js/hello_world.js
+++ samples/hello_world/public/js/hello_world.js
@@ -6,7 +6,7 @@
 
 /* specify the required XSLT stylesheets */
 //mvcimple_error.xsl and mvcimple_message.xsl should always be in the list
-xslt_sheets = ["mvimcple_error.xsl","mvcimple_message_xsl","add_greeting.xsl"];
+xslt_sheets = ["templates/mvcimple_error.xsl","templates/mvcimple_message.xsl","templates/add_greeting.xsl"];
 
 /*
 * function to initialize the page. At minimum this should download the xslt
@@ -33,16 +33,16 @@ function add_greeting()
     $.get("add",form_data,function(data)
                             {
                                 //show any errors we received
-                                mvcimple_show_data(data,"mvcimple_error.xsl","#add_greeting_result");
+                                mvcimple_show_data(data,"templates/mvcimple_error.xsl","#add_greeting_result");
                                 
                                 //if we got any errors, stop here
                                 if($("#add_greeting_result > .error").exists())
                                     return;
 
                                 //show any messages we received
-                                mvcimple_show_data(data,"mvcimple_message.xsl","#add_greeting_result");
+                                mvcimple_show_data(data,"templates/mvcimple_message.xsl","#add_greeting_result");
                                  
                                 //add another <li> into which we can put the new greeting
-                                mvcimple_show_data(data,"add_greeting.xsl");
+                                mvcimple_show_data(data,"templates/add_greeting.xsl");
                             });
 }//end add_greeting
